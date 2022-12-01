3ae9eccda425847ff72a60f4cb16b3055d02a5b5
dmitryjob4j <haoos@inbox.ru>
2022-12-01 17:19:06 +0300

    2. Обеспечить остановку потребителя. [#66825 #270313]

---
+++ src/main/java/ru/job4j/concurrent/waitnatify/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent.waitnatify;
+
+/**
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 01.12.2022
+ */
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    try {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            System.out.println(queue.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int i = 0; i != 3; i++) {
+                        try {
+                            queue.offer(i);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }).start();
+    }
+}
